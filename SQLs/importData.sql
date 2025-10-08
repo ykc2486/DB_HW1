297e508504ef66ca265ccb8a750e4482ce98c068
ykchiu2486 <roger2486.cs13@nycu.edu.tw>
2025-10-08 22:58:57 +0800

    smol change

--- SQLs/importData.sql
+++ SQLs/importData.sql
@@ -3,44 +3,30 @@
 
 BEGIN;
 
--------------------------------
 -- 1. themes
--------------------------------
 \copy themes FROM './archive/themes.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 2. part_categories
--------------------------------
 \copy part_categories FROM './archive/part_categories.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 3. colors
--------------------------------
 \copy colors FROM './archive/colors.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 4. sets
--------------------------------
 \copy sets FROM './archive/sets.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 5. parts
--------------------------------
 \copy parts FROM './archive/parts.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 6. inventories
--------------------------------
 \copy inventories FROM './archive/inventories.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
 -- 7. inventory_parts
--------------------------------
+
 \copy inventory_parts FROM './archive/inventory_parts.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
--------------------------------
+
 -- 8. inventory_sets
--------------------------------
 \copy inventory_sets FROM './archive/inventory_sets.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COMMIT;
